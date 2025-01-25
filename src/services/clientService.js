d79edab72570b5cc66e944d57a5228cc502e89b9
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-01-24 20:12:01 -0600

    Add credentials to the functions

--- src/services/clientService.js
+++ src/services/clientService.js
@@ -4,7 +4,7 @@ const API_URL = "https://backend-garment-store-bice.vercel.app/api/clients";
 
 export const getClients = async () => {
   try {
-    const response = await axios.get(API_URL);
+    const response = await axios.get(API_URL, { withCredentials: true });
     return response.data.data;
   } catch (error) {
     console.error(error);
@@ -14,7 +14,9 @@ export const getClients = async () => {
 
 export const createClient = async (clientData) => {
   try {
-    const response = await axios.post(API_URL, clientData);
+    const response = await axios.post(API_URL, clientData, {
+      withCredentials: true,
+    });
     return response.data.data;
   } catch (error) {
     console.error("Hubo un error al crear el cliente: ", error);
@@ -24,7 +26,9 @@ export const createClient = async (clientData) => {
 
 export const getClientById = async (id, clientData) => {
   try {
-    const response = await axios.get(`${API_URL}/${id}`);
+    const response = await axios.get(`${API_URL}/${id}`, {
+      withCredentials: true,
+    });
     return response.data.data;
   } catch (error) {
     console.error("Hubo un error al obtener el cliente: ", error);
@@ -34,7 +38,9 @@ export const getClientById = async (id, clientData) => {
 
 export const updateClient = (id, clientData) => {
   try {
-    const response = axios.put(`${API_URL}/${id}`, clientData);
+    const response = axios.put(`${API_URL}/${id}`, clientData, {
+      withCredentials: true,
+    });
     return response.data.data;
   } catch (error) {
     console.error("Hubo un error al actualizar el cliente: ", error);
@@ -44,7 +50,9 @@ export const updateClient = (id, clientData) => {
 
 export const deleteClient = async (id) => {
   try {
-    const response = await axios.delete(`${API_URL}/${id}`);
+    const response = await axios.delete(`${API_URL}/${id}`, {
+      withCredentials: true,
+    });
     return response.data.data;
   } catch (error) {
     console.error("Hubo un error al eliminar el cliente: ", error);
