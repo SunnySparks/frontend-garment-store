90c307eebc29f5e5edf772811d83efc969619063
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-01-24 20:00:35 -0600

    fix settings errors api clients

--- src/services/clientService.js
+++ src/services/clientService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://backend-garment-store-bice.vercel.app/clients";
+const API_URL = "https://backend-garment-store-bice.vercel.app/api/clients";
 
 export const getClients = async () => {
   try {
