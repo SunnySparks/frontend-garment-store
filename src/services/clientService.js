6bb48311921dfc7562db5f0ac203e3c27dee50e8
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-02-06 19:39:11 -0600

    fix api url

--- src/services/clientService.js
+++ src/services/clientService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "backend-garment-store-nine.vercel.app/api/clients";
+const API_URL = "https://backend-garment-store-nine.vercel.app/api/clients";
 
 export const getClients = async () => {
   try {
