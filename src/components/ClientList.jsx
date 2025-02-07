5dd455ca783d8b86a50eeabb7bddf9393a045709
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-02-06 19:38:42 -0600

    changes on client list

--- src/components/ClientList.jsx
+++ src/components/ClientList.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const ClientList = ({clients, onDelete}) => {
+const ClientList = ({ clients = [], onDelete }) => {
   return (
     <div>
-        <h2 className='text-xl font-bold mb-4'>Clientes</h2>
-        <ul>
-            {clients.map(client => (
-                <li key={client._id} className='mb-2'>
-                    <span>{client.name} - {client.garmentType}</span>
-                    <button onClick={() => onDelete(client._id)} className='text-red-500 px-2 py-1 rounded hover:text-red-700'>Borrar</button>
-                </li>
-            ))}
-        </ul>
+      <h2 className="text-xl font-bold mb-4">Clientes</h2>
+      <ul>
+        {clients.length > 0 ? (
+          clients.map((client) => (
+            <li key={client._id} className="mb-2">
+              <span>
+                {client.name} - {client.garmentType}
+              </span>
+              <button
+                onClick={() => onDelete(client._id)}
+                className="text-red-500 px-2 py-1 rounded hover:text-red-700"
+              >
+                Borrar
+              </button>
+            </li>
+          ))
+        ) : (
+          <p>No hay clientes</p>
+        )}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ClientList
+export default ClientList;
