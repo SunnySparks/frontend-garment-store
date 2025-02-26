a8f528753a778e312609977f59820dc7bb427719
José Francisco Silva Díaz <Josediazt6@outlook.com>
2025-02-25 20:16:40 -0600

    fix petition link

--- src/services/clientService.js
+++ src/services/clientService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = axios.create({
-  baseURL: "https://backend-garment-store-nine.vercel.app/api/clients",
+  baseURL: "https://backend-garment-store-nine.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
